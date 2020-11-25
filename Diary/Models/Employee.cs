c25406d4e34a2fcd0ad556181d741b01e65cc1af
Unbrocken <stas.chuhrai@gmail.com>
2020-11-25 20:04:34 +0200

    Class Employee + Db connection

---
+++ Diary/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Diary.Models
+{
+    public class Employee
+    {
+        public string Fname;
+        public string Lname;
+        public string Sname;
+        public string Position;
+        public string Department;
+        public string Event;
+        public string Status;
+        public int Hourly_Rate;
+        public int Many_hours_worked;
+        public string Photo;
+    }
+}
---
+++ Diary/Models/MobileContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Diary.Models
+{
+    public class MobileContext: DbContext
+    {
+        public DbSet<Employee> Employee { get; set; }
+
+       
+    }
+}
