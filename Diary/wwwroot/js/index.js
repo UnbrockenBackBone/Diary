e18dc48e0cac442a864fb050f9364d973f9fa636
Unbrocken <stas.chuhrai@gmail.com>
2020-12-25 22:22:33 +0200

    Last update

--- Diary/wwwroot/js/index.js
+++ Diary/wwwroot/js/index.js
@@ -1,6 +1,5 @@
 ﻿$(function () {
     $('.intro').addClass('go');
-
     $('.reload').click(function () {
         $('.intro').removeClass('go').delay(200).queue(function (next) {
             $('.intro').addClass('go');
