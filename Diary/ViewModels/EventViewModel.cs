9620f03b8556f160791f42cc4f9904c5f24d3178
UnbrockenBackBone <stas.chuhrai@gmail.com>
2020-12-26 12:29:54 +0200

    Created EventViewModel, and must to create EventView

---
+++ Diary/ViewModels/EventViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Diary.Models;
+
+namespace Diary.ViewModels
+{
+    public class EventViewModel
+    {
+        public IEnumerable<Event> Events { get; set; }
+        public IEnumerable<Event_Employee> Event_Employees {get;set;}
+    }
+}
